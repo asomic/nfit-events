2ec8020f05eafcd31a3afc8397790d1037c11968
ti-auditcon <ti@auditcon.cl>
2023-07-01 16:21:25 -0400

    fix

---
+++ app/Mail/EventForm.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class EventForm extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Get the modal contact data to send via email
+     *
+     * @var Illuminate\Http\Request
+     */
+    protected $request;
+
+    protected $country;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request, $country)
+    {
+        $this->request = $request;
+        $this->country = $country;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('contacto@nfit.app')
+                    ->view('web.emails.contact')
+                    ->with([
+                        'event' => $this->request->event,
+                        'name' => $this->request->name,
+                        'email' => $this->request->email,
+                        'box_center_name' => $this->request->box_center_name,
+                        'city' => $this->request->city,
+                        'students' => $this->request->students,
+                        'phone' => $this->request->phone,
+                        'country' => $this->country,
+                    ]);
+    }
+}
---
+++ resources/views/emails/event.blade.php
@@ -0,0 +1,32 @@
+<div>
+    Evento: {{ $event }}
+</div>
+<br>
+<div>
+    Nombre: {{ $name }}
+</div>
+<br>
+<div>
+    Email: {{ $email }}
+</div>
+<br>
+<div>
+    Telefono: {{ $phone }}
+</div>
+<br>
+<div>
+    Box: {{ $box_center_name }}
+</div>
+<br>
+<div>
+    Ciudad: {{ $city }}
+</div>
+<br>
+<div>
+    pais: {{ $country }}
+</div>
+<br>
+<div>
+    N° Usuarios: {{ $students }}
+</div>
+<br>
